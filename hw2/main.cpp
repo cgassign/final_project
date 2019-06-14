2e16a6b47cca26c41f58e088932c6b9bbf123068
MockingT <31817173+MockingT@users.noreply.github.com>
2019-06-14 22:07:45 +0800

    Add files via upload

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,40 +1,25 @@
-#include <glad\glad.h>
-#include <GLFW\glfw3.h>
-#include <assimp\Importer.hpp>
-#include <assimp\scene.h>
-#include <assimp\postprocess.h>
-#include <cmath>
-#include <algorithm>
+#include "fun.hpp"
 #include <vector>
 #include "imgui\imgui.h"
 #include "imgui\imgui_impl_glfw_gl3.h"
-#include "model.h"
-#include "camera.h"
-#include "shader.h"
-#include <iostream>
 
 #define bezier_point_num 1000
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
-unsigned int loadCubemap(vector<std::string> faces);
-
 // 设置窗口大小
-const unsigned int SCR_WIDTH = 1200;
-const unsigned int SCR_HEIGHT = 900;
+extern unsigned int SCR_WIDTH;
+extern unsigned int SCR_HEIGHT;
 
-// 摄像机
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
+extern float lastX, lastY;
 
-// 计时
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
+extern float deltaTime, lastFrame;
+
+extern glm::vec3 lightPos;
+
+extern unsigned int planeVAO, planeVBO;
+extern unsigned int lightVAO, lightVBO;
+extern unsigned int skyboxVAO, skyboxVBO;
+
 
 int main()
 {
@@ -44,66 +29,65 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	// 创建窗口对象
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLTest", NULL, NULL);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Project", NULL, NULL);
 	if (window == NULL)
 	{
-		cout << "Failed to create GLFW window" << endl;
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	// 告诉OpenGL渲染窗口的尺寸大小，用glViewport函数设置窗口维度
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//鼠标输入
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
-	// 让GFLW捕捉鼠标
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	// 在调用OpenGL函数前初始化GLAD
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	// 开启深度测试
+
+	const char* glsl_version = "#version 330 core";
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+
+	// Setup Platform/Renderer bindings
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	(void)io;
+	ImGui_ImplGlfwGL3_Init(window, true);
+	ImGui::StyleColorsDark();
+	
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
 	glEnable(GL_DEPTH_TEST);
-	// 构造shader
-	Shader ourShader("model_loading.vs", "model_loading.fs");
+
+	Shader shader("myShader/model.vs", "myShader/model.fs");
+	Shader simpleDepthShader("myShader/sample_depth.vs", "myShader/sample_depth.fs");
+	Shader lightShader("myShader/light.vs", "myShader/light.fs");
+
 	Shader skyboxShader("skybox.vs", "skybox.fs");
+
 	// 加载模型
 	Model tree("resource/tree/tree.obj");
 	Model player("resource/nanosuit/nanosuit.obj");
 	Model house1("resource/Building Apartment/Mesh/Building_Apartment_10.fbx");
 	Model house2("resource/Building Apartment/Mesh/Building_Apartment_13.fbx");
-	//Model ourModel("resource/TT_demo_police.FBX");
 
-	// 地板
-	float planeVertices[] = {
-		// positions            // normals         // texcoords
-		25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-		-25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
-		-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
 
-		25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-		-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
-		25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
-	};
-	// plane VBO
-	unsigned int planeVBO;
-	unsigned int planeVAO;
-	glGenVertexArrays(1, &planeVAO);
-	glGenBuffers(1, &planeVBO);
-	glBindVertexArray(planeVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glBindVertexArray(0);
+	//模型向量
+	vector<Model> allModels;
+	allModels.push_back(player);
+	allModels.push_back(tree);
+	allModels.push_back(house1);
+	allModels.push_back(house2);
+
 	// 导入地板的贴图
-	unsigned int woodTexture;// = loadTexture(FileSystem::getPath("resources/wood.png").c_str());
+	unsigned int woodTexture;
 	glGenTextures(1, &woodTexture);
 	int width, height, nrComponents;
 	unsigned char *data = stbi_load("resource/Ground/grass.tga", &width, &height, &nrComponents, 0);
@@ -133,61 +117,27 @@ int main()
 		stbi_image_free(data);
 	}
 
-	// 天空盒顶点数组
-	float skyboxVertices[] = {
-		// positions          
-		-25.0f,  25.0f, -25.0f,
-		-25.0f, -25.0f, -25.0f,
-		25.0f, -25.0f, -25.0f,
-		25.0f, -25.0f, -25.0f,
-		25.0f,  25.0f, -25.0f,
-		-25.0f,  25.0f, -25.0f,
-
-		-25.0f, -25.0f,  25.0f,
-		-25.0f, -25.0f, -25.0f,
-		-25.0f,  25.0f, -25.0f,
-		-25.0f,  25.0f, -25.0f,
-		-25.0f,  25.0f,  25.0f,
-		-25.0f, -25.0f,  25.0f,
-
-		25.0f, -25.0f, -25.0f,
-		25.0f, -25.0f,  25.0f,
-		25.0f,  25.0f,  25.0f,
-		25.0f,  25.0f,  25.0f,
-		25.0f,  25.0f, -25.0f,
-		25.0f, -25.0f, -25.0f,
-
-		-25.0f, -25.0f,  25.0f,
-		-25.0f,  25.0f,  25.0f,
-		25.0f,  25.0f,  25.0f,
-		25.0f,  25.0f,  25.0f,
-		25.0f, -25.0f,  25.0f,
-		-25.0f, -25.0f,  25.0f,
-
-		25.0f,  25.0f,  25.0f,
-		-25.0f,  25.0f, -25.0f,
-		-25.0f,  25.0f, 25.0f,
-		25.0f,  25.0f, -25.0f,
-		-25.0f,  25.0f, -25.0f,
-		25.0f,  25.0f,  25.0f,
-	
-
-		-25.0f, -25.0f, -25.0f,
-		-25.0f, -25.0f,  25.0f,
-		25.0f, -25.0f, -25.0f,
-		25.0f, -25.0f, -25.0f,
-		-25.0f, -25.0f,  25.0f,
-		25.0f, -25.0f,  25.0f
-	};
-
-	unsigned int skyboxVAO, skyboxVBO;
-	glGenVertexArrays(1, &skyboxVAO);
-	glGenBuffers(1, &skyboxVBO);
-	glBindVertexArray(skyboxVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//生成深度贴图
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	unsigned int depthMapFBO;
+	glGenFramebuffers(1, &depthMapFBO);
+	//创建2D纹理
+	unsigned int depthMap;
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	//生成的深度纹理作为帧缓冲的深度缓冲
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 	// 天空盒贴图
 	vector<std::string> faces
@@ -201,67 +151,102 @@ int main()
 	};
 	unsigned int cubemapTexture = loadCubemap(faces);
 
-	//渲染循环
+	shader.use();
+	shader.setInt("diffuseTexture", 0);
+	shader.setInt("shadowMap", 1);
+
+	bool isAuto = false;
+	bool isOrtho = true;
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// 获取每帧时间
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		// 输入
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
 		processInput(window);
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwPollEvents();
+		glm::mat4 lightProjection, lightView;
+		glm::mat4 lightSpaceMatrix;
+		float near_plane = 1.0f, far_plane = 7.5f;
 
-		ourShader.use();
+		// Start the Dear ImGui frame
+		ImGui_ImplGlfwGL3_NewFrame();
+		ImGui::SetWindowFontScale(1.4);
 
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera.GetViewMatrix();
-		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
+		ImGui::Text("Projection");
+		ImGui::Checkbox("Orthogonal projection", &isOrtho);
+		ImGui::Separator();
 
-		// 渲染模型
-		glm::mat4 plane_model = glm::mat4(1.0f);
-		// 渲染平面
-		plane_model = glm::translate(plane_model, glm::vec3(0.0f, -1.5f, 0.0f));
-		ourShader.setMat4("model", plane_model);
+		ImGui::Text("LightSource position");
+		ImGui::SliderFloat("LightSource.x", &lightPos.x, -10.0f, 10.0f);
+		ImGui::SliderFloat("LightSource.y", &lightPos.y, 0.0f, 10.0f);
+		ImGui::SliderFloat("LightSource.z", &lightPos.z, -10.0f, 10.0f);
+		ImGui::Checkbox("Automatic rotation", &isAuto);
+		ImGui::Separator();
+
+		if (isAuto) {
+			lightPos.x = 2 * sin(glfwGetTime()) * 1.0f;
+			lightPos.z = 2 * cos(glfwGetTime()) * 1.0f;
+		}
+		if (isOrtho) {
+			lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+		}
+		else {
+			lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane);
+		}
+		//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		//ImGui::End();
+	
+
+		//物体及阴影
+		lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+		lightSpaceMatrix = lightProjection * lightView;
+		// render scene from light's point of view
+		simpleDepthShader.use();
+		simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, woodTexture);
-		glBindVertexArray(planeVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// 正面剔除，消除peter游移
+		glCullFace(GL_FRONT);
+		renderScene(simpleDepthShader, allModels);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glCullFace(GL_BACK);
 
-		// 调整树的位置
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(-1.0f, -1.5f, -2.0f));
-		model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));	
-		ourShader.setMat4("model", model);
-		tree.Draw(ourShader);
-		model = glm::translate(model, glm::vec3(40.0f, 0.0f, 0.0f));
-		ourShader.setMat4("model", model);
-		tree.Draw(ourShader);
+		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// 调整房屋的位置
-		glm::mat4 house_model = glm::mat4(1.0f);
-		house_model = glm::translate(house_model, glm::vec3(5.0f, -1.5f, -8.0f));
-		house_model = glm::scale(house_model, glm::vec3(0.0025f, 0.0025f, 0.0025f));
-		ourShader.setMat4("model", house_model);
-		house1.Draw(ourShader);
+		shader.use();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
 
-		glm::mat4 house_model2 = glm::mat4(1.0f);
-		house_model2 = glm::translate(house_model2, glm::vec3(-3.0f, -1.5f, -5.0f));
-		house_model2 = glm::scale(house_model2, glm::vec3(0.0025f, 0.0025f, 0.0025f));
-		ourShader.setMat4("model", house_model2);
-		house2.Draw(ourShader);
+		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
+		// set light uniforms	
+		shader.setVec3("viewPos", camera.Position);
+		shader.setVec3("lightPos", lightPos);
+		shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, woodTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		renderScene(shader, allModels);
 
-		glm::mat4 player_model = glm::mat4(1.0f);
-		// 调整人物的位置
-		player_model = glm::translate(player_model, glm::vec3(0.0f, -1.5f, 0.0f));
-		player_model = glm::scale(player_model, glm::vec3(0.05f, 0.05f, 0.05f));
-		player_model = glm::rotate(player_model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		ourShader.setMat4("model", player_model);
-		player.Draw(ourShader);
-
-		// 画天空盒
+		//光源
+		lightShader.use();
+		glm::mat4 model = glm::mat4(1.0);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
+		lightShader.setMat4("model", model);
+		lightShader.setMat4("projection", projection);
+		lightShader.setMat4("view", view);
+		renderLight();
+		
+		//天空盒
 		// 更改深度函数，以便深度测试在值等于深度缓冲区的内容时通过
 		glDepthFunc(GL_LEQUAL);
 		skyboxShader.use();
@@ -269,98 +254,33 @@ int main()
 		view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
 		skyboxShader.setMat4("view", view);
 		skyboxShader.setMat4("projection", projection);
-		// 天空盒
 		glBindVertexArray(skyboxVAO);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		glBindVertexArray(0);
+		renderSkybox();
 		// 深度设置回默认值
 		glDepthFunc(GL_LESS);
 
+		// Rendering
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+		glfwMakeContextCurrent(window);
 		glfwSwapBuffers(window);
-		glfwPollEvents();
 	}
+	// Cleanup
+	
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+	
+
+	glDeleteVertexArrays(1, &planeVAO);
+	glDeleteBuffers(1, &planeVBO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &lightVBO);
+	glDeleteVertexArrays(1, &skyboxVAO);
+	glDeleteBuffers(1, &skyboxVBO);
 	glfwTerminate();
-
 	return 0;
-}
-
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
-}
-
-// 当窗口大小变化，调用这个函数
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	// 确保视点和窗口大小匹配
-	glViewport(0, 0, width, height);
-}
-
-// 鼠标移动的回调函数
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; 
-
-	lastX = xpos;
-	lastY = ypos;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// 鼠标滚轮的回调函数
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	camera.ProcessMouseScroll(yoffset);
-}
-
-// 加载天空盒代码
-unsigned int loadCubemap(vector<std::string> faces)
-{
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
-
-	int width, height, nrChannels;
-	for (unsigned int i = 0; i < faces.size(); i++)
-	{
-		unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
-			);
-			stbi_image_free(data);
-		}
-		else
-		{
-			std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
-			stbi_image_free(data);
-		}
-	}
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-	return textureID;
 }
